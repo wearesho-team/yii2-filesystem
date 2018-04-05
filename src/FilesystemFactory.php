bd461c9f90c82eba62b57be05726f746bca19abe
Alexander Letnikow <reclamme@gmail.com>
2018-04-05 17:39:10 +0300

    Use alias instead of url manager in FileSystem factory

--- src/FilesystemFactory.php
+++ src/FilesystemFactory.php
@@ -8,7 +8,6 @@ use League\Flysystem\AwsS3v3\AwsS3Adapter;
 use League\Flysystem\Adapter\Local as LocalAdapter;
 
 use yii\di\Container;
-use yii\web\UrlManager;
 
 /**
  * Class FilesystemFactory
@@ -38,10 +37,7 @@ class FilesystemFactory
             $baseUrl = getenv('S3_BASE_URL');
         } else {
             $adapter = new LocalAdapter(\Yii::getAlias('@fileStorage'));
-            
-            /** @var UrlManager $manager */
-            $manager = $container->get(UrlManager::class);
-            $baseUrl = $manager->createUrl('/');
+            $baseUrl = \Yii::getAlias('@fileStorageUrl', false) ?: '';
         }
 
         return new Filesystem($adapter, $baseUrl);
