f9392aafa507265961e9a6bd5bf32629ddf485dc
Alexander Letnikow <reclamme@gmail.com>
2018-06-27 15:55:11 +0300

    Implement adapter interface using Ftp adapter

---
+++ src/Ftp/ConfigInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Wearesho\Yii\Filesystem\Ftp;
+
+/**
+ * Interface ConfigInterface
+ * @package Wearesho\Yii\Filesystem\Ftp
+ */
+interface ConfigInterface
+{
+    public function getHost(): string;
+
+    public function getUser(): string;
+
+    public function getPassword(): string;
+
+    public function getPort(): int;
+
+    public function getPrefix(): string;
+
+    public function getBaseUrl(): string;
+}
