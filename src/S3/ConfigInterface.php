622f647607e1c94b49694b1d3b0254f5e08aa133
Alexander Letnikow <reclamme@gmail.com>
2018-06-27 15:45:08 +0300

    Implement adapter interface using AwsS3Adapter

---
+++ src/S3/ConfigInterface.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Wearesho\Yii\Filesystem\S3;
+
+/**
+ * Interface ConfigInterface
+ * @package Wearesho\Yii\Filesystem
+ */
+interface ConfigInterface
+{
+    public function getEndpoint(): string;
+
+    public function getKey(): string;
+
+    public function getSecret(): string;
+
+    public function getVersion(): string;
+
+    public function getRegion(): string;
+
+    public function getBucket(): string;
+
+    public function getBaseUrl(): string;
+
+    public function getPrefix(): string;
+}
---
+++ src/S3/EnvironmentConfig.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Wearesho\Yii\Filesystem\S3;
+
+use Horat1us\Environment;
+
+/**
+ * Class EnvironmentConfig
+ * @package Wearesho\Yii\Filesystem
+ */
+class EnvironmentConfig extends Environment\Yii2\Config implements ConfigInterface
+{
+    public $keyPrefix = 'S3_';
+
+    public function getEndpoint(): string
+    {
+        return $this->getEnv('ENDPOINT');
+    }
+
+    public function getKey(): string
+    {
+        return $this->getENv('KEY');
+    }
+
+    public function getSecret(): string
+    {
+        return $this->getEnv('SECRET');
+    }
+
+    public function getVersion(): string
+    {
+        return $this->getEnv('VERSION');
+    }
+
+    public function getRegion(): string
+    {
+        return $this->getEnv('REGION');
+    }
+
+    public function getBucket(): string
+    {
+        return $this->getEnv('BUCKET');
+    }
+
+    /**
+     * @return string
+     * @todo: implement default value using bucket, endpoint methods
+     */
+    public function getBaseUrl(): string
+    {
+        return $this->getEnv('BASE_URL');
+    }
+
+    public function getPrefix(): string
+    {
+        return $this->getEnv('PREFIX', '');
+    }
+}
