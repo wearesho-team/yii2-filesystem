d1844e190b3fda5f8fef86b39e822f8eabc484db
Alexander Letnikow <reclamme@gmail.com>
2018-06-27 15:45:08 +0300

    Implement adapter interface using AwsS3Adapter

---
+++ src/S3/AdapterInterface.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Wearesho\Yii\Filesystem\S3;
+
+use yii\base;
+use yii\di;
+use Aws\S3\S3Client;
+use League\Flysystem;
+use Wearesho\Yii\Filesystem;
+
+/**
+ * Class Adapter
+ * @package Wearesho\Yii\Filesystem\S3
+ */
+class AdapterInterface extends Flysystem\AwsS3v3\AwsS3Adapter implements Filesystem\AdapterInterface, base\Configurable
+{
+    /** @var array|string|ConfigInterface definition */
+    public $config = [
+        'class' => EnvironmentConfig::class,
+    ];
+
+    /**
+     * Adapter constructor.
+     * @param array $config
+     * @throws base\InvalidConfigException
+     */
+    public function __construct(
+        array $config = []
+    ) {
+        \Yii::configure($this, $config);
+        $this->config = di\Instance::ensure($this->config, ConfigInterface::class);
+
+        $bucket = $this->config->getBucket();
+        $prefix = $this->config->getPrefix();
+        $client = new S3Client([
+            'endpoint' => $this->config->getEndpoint(),
+            'credentials' => [
+                'key' => $this->config->getKey(),
+                'secret' => $this->config->getSecret(),
+            ],
+            'version' => $this->config->getVersion(),
+            'region' => $this->config->getRegion(),
+        ]);
+
+        parent::__construct($client, $bucket, $prefix, $this->options);
+    }
+
+    public function getBaseUrl(): string
+    {
+        return $this->config->getBaseUrl();
+    }
+}
