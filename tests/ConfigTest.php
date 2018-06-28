a9071fe59df073eee849925656b58780e59a6150
Alexander Letnikow <reclamme@gmail.com>
2018-06-28 15:07:27 +0300

    Add test for Filesystem\Config

---
+++ tests/ConfigTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Wearesho\Yii\Filesystem\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Wearesho\Yii\Filesystem;
+
+/**
+ * Class ConfigTest
+ * @package Wearesho\Yii\Filesystem\Tests
+ */
+class ConfigTest extends TestCase
+{
+    /** @var Filesystem\Config */
+    protected $config;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->config = new Filesystem\Config();
+    }
+
+    public function testGetAdapter(): void
+    {
+        $this->assertEquals(
+            's3',
+            $this->config->getAdapter()
+        );
+        $this->config->adapter = 'default';
+        $this->assertEquals(
+            'default',
+            $this->config->getAdapter()
+        );
+    }
+}
