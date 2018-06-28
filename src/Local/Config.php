2771109bf1be3df53faf5e041495da7e4facb44c
Alexander Letnikow <reclamme@gmail.com>
2018-06-28 14:58:11 +0300

    Implement Config entity for Local\ConfigInterface

---
+++ src/Local/Config.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Wearesho\Yii\Filesystem\Local;
+
+use yii\base;
+
+/**
+ * Class Config
+ * @package Wearesho\Yii\Filesystem\Local
+ */
+class Config extends base\BaseObject implements ConfigInterface
+{
+    /** @var string */
+    public $savePath = '@runtime';
+
+    /** @var string|null */
+    public $baseUrl = null;
+
+    public function getSavePath(): string
+    {
+        return \Yii::getAlias($this->savePath);
+    }
+
+    /**
+     * @return string
+     * @throws base\InvalidConfigException
+     */
+    public function getBaseUrl(): string
+    {
+        if (is_null($this->baseUrl)) {
+            throw new base\InvalidConfigException("basePath is not configured");
+        }
+
+        return \Yii::getAlias($this->baseUrl);
+    }
+}
