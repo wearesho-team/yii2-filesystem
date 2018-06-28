51ba6e129ceed944d616e6b4ff45573e3ea92bcc
Alexander Letnikow <reclamme@gmail.com>
2018-06-28 15:00:09 +0300

    Refactor Filesystem to implement base\Configurable interface

--- src/Filesystem.php
+++ src/Filesystem.php
@@ -2,19 +2,33 @@
 
 namespace Wearesho\Yii\Filesystem;
 
+use yii\base;
+use yii\di;
 use League\Flysystem;
 
 /**
  * Class Filesystem
  * @package Wearesho\Yii\Filesystem
  *
- * @property-read AdapterInterface $adapter
+ * @property AdapterInterface $adapter
  */
-class Filesystem extends Flysystem\Filesystem
+class Filesystem extends Flysystem\Filesystem implements base\Configurable
 {
-    public function __construct(AdapterInterface $adapter, $config = null)
+    /**
+     * Filesystem constructor.
+     * @param array $config
+     * @throws base\InvalidConfigException
+     */
+    public function __construct(array $config = [])
     {
-        parent::__construct($adapter, $config);
+        if (!array_key_exists('adapter', $config)) {
+            throw new base\InvalidConfigException("Adapter have to specified in config");
+        }
+
+        /** @var AdapterInterface $adapter */
+        $adapter = di\Instance::ensure($config['adapter'], AdapterInterface::class);
+
+        parent::__construct($adapter, $config['config'] ?? null);
     }
 
     public function getUrl(string $path): string
