39d713e165fd9695fb0d61095237f19c5b9b7d4b
Alexander Letnikow <reclamme@gmail.com>
2019-02-27 18:38:52 +0200

    Implement encoding url path components

--- tests/FilesystemTest.php
+++ tests/FilesystemTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Wearesho\Yii\Filesystem\Tests;
 
 use PHPUnit\Framework\TestCase;
@@ -33,12 +35,12 @@ class FilesystemTest extends TestCase
      * @expectedException \yii\base\InvalidConfigException
      * @expectedExceptionMessage Adapter have to specified in config
      */
-    public function testMissingAdapter()
+    public function testMissingAdapter(): void
     {
         new Filesystem\Filesystem([]);
     }
 
-    public function testGettingUrl()
+    public function testGettingUrl(): void
     {
         $fs = new Filesystem\Filesystem([
             'adapter' => [
@@ -53,6 +55,24 @@ class FilesystemTest extends TestCase
         $this->assertEquals(
             'https://wearesho.com/404.html',
             $fs->getUrl('404.html')
+        );
+    }
+
+    public function testGettingUrlWithPathEncoding(): void
+    {
+        $fs = new Filesystem\Filesystem([
+            'adapter' => [
+                'class' => Filesystem\Local\Adapter::class,
+                'config' => [
+                    'class' => Filesystem\Local\Config::class,
+                    'baseUrl' => 'https://wearesho.com/',
+                ],
+            ],
+        ]);
+
+        $this->assertEquals(
+            'https://wearesho.com/documents/137328/c%23omments/req2%23000163540245.pdf',
+            $fs->getUrl('/documents/137328/c#omments/req2#000163540245.pdf')
         );
     }
 }
